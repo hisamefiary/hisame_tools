0ab2acd7f5a778a4808cba176b5f44340d77dec0
hisamefiary <reina51515@gmail.com>
2025-06-22 19:31:04 +0900

    Delete main/general.js

--- main/general.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function create(tagname, innertext, parent) {
-  let element = document.createElement(tagname)
-  if (innertext) {
-    element.innerHTML = innertext;
-  }
-  parent.appendChild(element)
-  return element
-}
